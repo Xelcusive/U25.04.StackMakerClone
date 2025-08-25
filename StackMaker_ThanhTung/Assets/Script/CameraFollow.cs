bfb68b6b60a4f50b8c332fb950f01750a0910ed4
Xelcusive <binz30244@gmail.com>
2025-08-25 08:02:23 +0700

    ~fix

---
+++ StackMaker_ThanhTung/Assets/Script/CameraFollow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float value;
+  
+    private void LateUpdate()
+    {
+        Vector3 pos = target.position +offset;
+
+        transform.position = Vector3.Lerp(transform.position, pos, value);
+    }
+}
